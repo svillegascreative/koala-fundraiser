3887a4d57a53807dd958a95a256b2a0e15197cd6
sylvia villegas <sylvia@svillegas.com>
2020-01-07 11:02:30 -0500

    Create gallery page

---
+++ pages/gallery.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+	AppGallery
+</template>
+
+<script>
+import AppGallery from '~/components/AppGallery'
+
+export default {
+  name: 'Gallery',
+  components: {
+    AppGallery
+  }
+}
+</script>
+
+<style></style>
