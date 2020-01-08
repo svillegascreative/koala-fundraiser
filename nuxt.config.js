90b3a73abf95946a0ce509fab7bc32c199a52b85
sylvia villegas <sylvia@svillegas.com>
2020-01-08 10:12:34 -0500

    Add HTML lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    htmlAttrs: { lang: 'en' }
   },
   /*
    ** Customize the progress-bar color
