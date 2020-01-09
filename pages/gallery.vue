a3c097ea4b2d3a57a8c31edd892ea7c13f4d48cd
sylvia villegas <sylvia@svillegas.com>
2020-01-09 11:51:00 -0500

    Tinker with gallery asyncData...

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -11,11 +11,37 @@ export default {
   components: {
     AppGallery
   },
+  data() {
+    return {
+      images: []
+    }
+    // },
+    // async asyncData() {
+    //   const { data } = await axios.get(
+    //     `https://res.cloudinary.com/svillegas/image/list/koala_fundraiser.json`
+    //   )
+    //   return { images: data.resources }
+    // },
+    // async asyncData() {
+    //   const { data } = await axios.get(
+    //     `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/svillegas/resources/image/tags/koala_fundraiser?max_results=60`
+    //   )
+    //   return { images: data.resources }
+  },
   async asyncData() {
-    const { data } = await axios.get(
-      `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/svillegas/resources/image/tags/koala_fundraiser?max_results=60`
-    )
-    return { images: data.resources }
+    try {
+      await axios({
+        method: 'GET',
+        url: `${FUNCTIONS_ENDPOINT}/cloudinary-resources-by-tag`,
+        headers: { 'Content-Type': 'application/json' }
+      }).then((res) => {
+        if (res.status === 200) {
+          return { images: res.resources }
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 </script>
