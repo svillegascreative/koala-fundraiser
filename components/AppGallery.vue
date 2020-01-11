54e52bf7fea8845c9d09cb45d396305f912d85e3
sylvia villegas <sylvia@svillegas.com>
2020-01-11 12:41:50 -0500

    Link gallery thumbnails to image pages

--- components/AppGallery.vue
+++ components/AppGallery.vue
@@ -1,13 +1,15 @@
 <template lang="pug">
-		div
-			ul.grid
-				li(v-for="image in images")
-					CldImage(cloud_name="svillegas" :publicId="image.public_id" loading="lazy")
-						CldTransformation(width="200" height="200" crop="fit" quality="auto" fetch_format="auto" dpr="auto")
+  div
+    ul.grid
+      li(v-for="image in images")
+        nuxt-link(:to="`/image/${image.id}`")
+          CldImage(cloud_name="svillegas" :publicId="image.public_id" loading="lazy")
+            CldTransformation(width="200" height="200" crop="fit" quality="auto" fetch_format="auto" dpr="auto")
 	
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import { CldImage, CldTransformation } from 'cloudinary-vue'
 
 export default {
@@ -16,7 +18,10 @@ export default {
     CldImage,
     CldTransformation
   },
-  props: { images: Array }
+  props: { images: Array },
+  computed: {
+    ...mapGetters(['imageID'])
+  }
 }
 </script>
 
