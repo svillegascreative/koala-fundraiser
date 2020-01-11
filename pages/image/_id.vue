90676d796a9a82317457b0e053309417aafe2e7a
sylvia villegas <sylvia@svillegas.com>
2020-01-11 12:39:50 -0500

    Create dynamic image page

---
+++ pages/image/_id.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+	section
+		img(:src="image.url")
+
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  // data() {
+  //   return {
+  //     id: this.$route.params.id
+  //   }
+  // },
+  computed: {
+    ...mapState(['galleryData']),
+    image() {
+      return this.galleryData.find(
+        (image) => image.id === this.$route.params.id
+      )
+    }
+  }
+}
+</script>
+
+<style></style>
