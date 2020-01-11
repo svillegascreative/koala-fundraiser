dea416d8f2e6334f38dcb67b9f89acfe7fba8f81
sylvia villegas <sylvia@svillegas.com>
2020-01-10 22:41:29 -0500

    Copy  Sarah's getters and create fake cart items

--- store/index.js
+++ store/index.js
@@ -2,12 +2,31 @@ import galleryData from '~/static/gallerydata.json'
 
 export const state = () => ({
   galleryData,
-  cart: []
+  cart: [
+    {
+      id: 1,
+      img: 'url',
+      size: 'A5 print (inches)',
+      price: '40.00',
+      qty: 2
+    },
+    {
+      id: 2,
+      img: 'url',
+      size: 'A4 print (inches)',
+      price: '50.00',
+      qty: 1
+    }
+  ]
 })
 
 export const getters = {
   cartCount: (state) => {
     if (!state.cart.length) return 0
-    return state.cart.reduce((ac, next) => ac + next.quantity, 0)
+    return state.cart.reduce((ac, next) => ac + next.qty, 0)
+  },
+  cartTotal: (state) => {
+    if (!state.cart.length) return 0
+    return state.cart.reduce((ac, next) => ac + next.qty * next.price, 0)
   }
 }
