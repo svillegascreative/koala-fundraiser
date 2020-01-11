59ce0dde3023d5ed809d2c88802d0ee12837b7ad
sylvia villegas <sylvia@svillegas.com>
2020-01-10 22:42:20 -0500

    Create AppCartContents comp't

---
+++ components/AppCartContents.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+	div
+		section(v-if="cartCount === 0")
+			p Your cart is empty. Check out the #[nuxt-link(to="/gallery") gallery] to select from available prints.
+	
+		section(v-else)
+			table
+				tr
+					th Print
+					th Price
+					th Quantity	
+					th Cost
+				tr(v-for="item in cart" :key="item.id")
+					td
+						img(:src="item.img" alt="print image")
+						| {{item.size}}
+					td
+						| {{item.price | dollar}}
+					td
+						| {{item.qty}}
+					td
+						| {{item.price * item.qty | dollar}}
+				tr
+					td(colspan="2")
+					td.font-semibold Total:
+					td.font-semibold {{cartTotal | dollar}}
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['cart']),
+    ...mapGetters(['cartCount', 'cartTotal'])
+  }
+}
+</script>
+
+<style></style>
