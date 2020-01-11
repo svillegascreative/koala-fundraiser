dc73ac777a577868dc2a794c623ee400bbfd18fb
sylvia villegas <sylvia@svillegas.com>
2020-01-10 20:34:28 -0500

    Add cart to store

--- store/index.js
+++ store/index.js
@@ -1,5 +1,13 @@
 import galleryData from '~/static/gallerydata.json'
 
 export const state = () => ({
-  galleryData
+  galleryData,
+  cart: []
 })
+
+export const getters = {
+  cartCount: (state) => {
+    if (!state.cart.length) return 0
+    return state.cart.reduce((ac, next) => ac + next.quantity, 0)
+  }
+}
