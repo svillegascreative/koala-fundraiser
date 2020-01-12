5bc862e80f510535ddf269b10cdb380044efee4f
sylvia villegas <sylvia@svillegas.com>
2020-01-11 20:32:27 -0500

    Add content and styles on image page

--- pages/image/_id.vue
+++ pages/image/_id.vue
@@ -1,18 +1,31 @@
 <template lang="pug">
-	section
-		img(:src="image.url")
-
+  div
+    section.max-w-5xl.mx-auto
+      img(:src="image.url").w-full
+      p.font-semibold {{`#${this.id}`}}
+    table.mx-auto
+      tbody
+        tr(v-for="product in products")
+          td.pb-3.text-right {{product.name}} ({{product.size}})
+          td.pb-3.text-center
+            input(type="number" min="0" v-model="qty").w-16
+        tr
+          td
+          td
+            button(@click="updateCart") Add to cart
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import products from '~/static/products'
 
 export default {
-  // data() {
-  //   return {
-  //     id: this.$route.params.id
-  //   }
-  // },
+  data() {
+    return {
+      products,
+      id: this.$route.params.id
+    }
+  },
   computed: {
     ...mapState(['galleryData']),
     image() {
