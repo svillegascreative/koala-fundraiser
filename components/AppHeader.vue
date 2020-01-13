6a91a073b34068b4ac52d10a1f3607406ad1a890
sylvia villegas <sylvia@svillegas.com>
2020-01-12 20:34:28 -0500

    Add cart badge on header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -7,13 +7,19 @@
 				nuxt-link(to="/orders") Orders
 			li.p-3
 				nuxt-link(to="/gallery") Gallery
-			li.p-3
+			li.p-3.relative
 				nuxt-link(to="/cart") Cart
+				span(v-if="cartCount > 0").badge {{cartCount}}
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  computed: {
+    ...mapGetters(['cartCount'])
+  }
 }
 </script>
 
@@ -25,5 +31,21 @@ li {
 li:hover {
   @apply bg-green-200;
   transition: 0.3s all ease;
+}
+
+.badge {
+  @apply bg-green-300;
+  @apply text-green-900;
+  @apply text-xs;
+  @apply font-semibold;
+  @apply rounded-full;
+  @apply h-4;
+  @apply w-4;
+  @apply flex;
+  @apply justify-center;
+  @apply items-center;
+  @apply absolute;
+  top: 0.5rem;
+  right: -0.75rem;
 }
 </style>
