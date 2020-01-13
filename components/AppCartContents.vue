41003615422e6cec50fd0dd8def123a35ff11ba7
sylvia villegas <sylvia@svillegas.com>
2020-01-13 00:01:21 -0500

    Style the cart contents

--- components/AppCartContents.vue
+++ components/AppCartContents.vue
@@ -1,31 +1,35 @@
 <template lang="pug">
 	div
-		section(v-if="cartCount === 0")
+		section(v-if="cartCount === 0").max-w-lg.mx-auto
 			p Your cart is empty. Check out the #[nuxt-link(to="/gallery") gallery] to select from available prints.
 	
-		section(v-else)
+		section(v-else).max-w-lg.mx-auto
 			table
 				thead
 					tr
-						th Print
-						th Price
-						th Quantity	
-						th Cost
+						th.border-b.border-gray-800.px-3 Print
+						th.border-b.border-gray-800.px-3 Price
+						th.border-b.border-gray-800.px-3 Quantity	
+						th.border-b.border-gray-800.px-3 Cost
 				tbody
 					tr(v-for="item in cart" :key="item.id")
-						td
-							img(:src="item.img" alt="print image")
-							| {{item.size}}
-						td
+						td.border-b.border-gray-400.py-3.px-3
+							div.flex.justify-center.items-center
+								img(:src="item.url" alt="thumbnail of this print").thumb.object-contain
+							span.font-semibold {{`#${item.id}`}}: 
+							span {{item.name}} ({{item.size}})
+						td.border-b.border-gray-400.py-3.text-center
 							| {{item.price | dollar}}
-						td
+						td.border-b.border-gray-400.py-3.text-center
 							| {{item.qty}}
-						td
+						td.border-b.border-gray-400.py-3.text-center
 							| {{item.price * item.qty | dollar}}
 					tr
-						td(colspan="2")
-						td.font-semibold Total:
-						td.font-semibold {{cartTotal | dollar}}
+						td(colspan="2").font-semibold.text-lg.text-right Total:
+						td.font-semibold.text-lg.text-center.leading-none {{cartCount}}
+							br
+							span.text-sm.leading-none items
+						td.font-semibold.text-lg.text-center {{cartTotal | dollar}}
 </template>
 
 <script>
@@ -39,4 +43,16 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.thumb {
+  max-width: 6rem;
+  max-height: 6rem;
+}
+
+@media (min-width: 30rem) {
+  .thumb {
+    max-width: 10rem;
+    max-height: 10rem;
+  }
+}
+</style>
